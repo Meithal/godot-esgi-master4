16588104af239b586979e64160aefcc1588eb2d9
Meithal <hurin8888@gmail.com>
2025-09-30 10:42:59 +0200

    affiche barres de differentes tailles

--- core-dotnet/FlappyCore.Tests/Test1.cs
+++ core-dotnet/FlappyCore.Tests/Test1.cs
@@ -3,10 +3,36 @@
 [TestClass]
 public sealed class TestToto
 {
+    private readonly ClassFlappy _classFlappy = new ClassFlappy();
+
     [TestMethod]
     public void TestTotoWorks()
     {
 
         Assert.AreEqual("toto", ClassFlappy.Toto());
+    }
+
+    [TestMethod]
+    public void TestObstacles()
+    {
+        {
+            double accum = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                accum += _classFlappy.GetObstacle(i);
+
+            }
+            Assert.AreEqual(0, accum);
+        }
+        _classFlappy.GenerateObstaclesValues(new Random().Next());
+        {
+            double accum = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                accum += _classFlappy.GetObstacle(i);
+
+            }
+            Assert.AreNotEqual(0, accum);
+        }
     }
 }
--- core-dotnet/FlappyCore/ClassFlappy.cs
+++ core-dotnet/FlappyCore/ClassFlappy.cs
@@ -2,8 +2,31 @@
 
 public class ClassFlappy
 {
+    #region Toto
     public static string Toto()
     {
         return "toto";
     }
+
+    #endregion
+
+    #region Flappy
+    private readonly double[] _obstacles = new double[100];
+
+    public void GenerateObstaclesValues(int seed)
+    {
+        var rand = new Random(seed);
+
+        for (int i = 0; i < 100; i++)
+        {
+            _obstacles[i] = rand.NextDouble();
+        }
+    }
+
+    public double GetObstacle(int which)
+    {
+        return _obstacles[which];
+    }
+
+#endregion
 }
