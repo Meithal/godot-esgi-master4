d697a1384d50659ba0cfec07e1fc0acff0721e74
Meithal <hurin8888@gmail.com>
2025-09-30 10:40:56 +0200

    affiche barres de differentes tailles

--- godot/Root.cs
+++ godot/Root.cs
@@ -2,22 +2,39 @@ using Godot;
 using System;
 using FlappyCore;
 
-public partial class Root : Node3D
+public partial class Root : Node2D
 {
 
 	[Export] private float _speed = 1f;
 	private float time = 1f;
 
+	private ClassFlappy _classFlappy = new ClassFlappy();
+
 	public override void _Ready()
 	{
 		GD.Print("Mon comp ready");
 		GD.Print("Mon comp ready2");
-		GD.Print(FlappyCore.ClassFlappy.Toto());
+		GD.Print(ClassFlappy.Toto());
+
+		_classFlappy.GenerateObstaclesValues(new Random().Next());
+
+		var canvas = GetNode<ColorRect>("%Canvas");
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				var bar = new ColorRect();
+				double value = _classFlappy.GetObstacle(i);
+				bar.Color = Colors.Red;
+				bar.Size = new Vector2(8, (int)(100 * value));
+				bar.Position = new Vector2(i * 10, 0);
+				canvas.AddChild(bar);
+			}
+		}
 	}
 
 	public override void _Process(double delta)
 	{
-
+		// GD.Print("Mon comp ready");
 		// this.Translate();
 	}
 }
